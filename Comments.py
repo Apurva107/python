2168821f8b70cd259fedcc72f145b23d1f0b28f9
Apurva107 <apurvapatil71@gmail.com>
2025-12-01 18:16:14 +0100

    Added python files

---
+++ Comments.py
@@ -0,0 +1,21 @@
+# Comments 
+# A note in code, used to write something which programm doesn't execute. 
+# Comments are ignored by Python when the code runs.
+# They’re just for humans — to explain, organize, and document code.
+
+#Types of Comments 
+# 1.Single line comment '#'
+# 2.Multi-line comment ''' comment'''
+
+#Why to use Comment
+# 1.Understandable 
+# 2.Readable
+# 3.Professional
+
+#Where we place comments 
+
+#Store the value (comment above the code for long explanation)
+X = 10 
+
+X = 9 # This is called inline comment at the end of the code (short explanation)
+print(X)
---
+++ Modules & pip.py
@@ -0,0 +1,11 @@
+# Modules & Pip in Python
+
+#Modules - Is a file containing code written by somebody else which can be later imported and can be used in  our program.
+
+# Types of Modules
+# 1. Built-in Modules - Pre installed in python.
+# 2. External Modules - They are imported from third party file need to be installed by using pip.
+
+#Pip command - It is the package manager for python it is used to install a module.
+# Ex: Install moule called pandas
+# pip install pandas
