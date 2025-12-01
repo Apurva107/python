3ef668207d1f2d2e876d8ea6e95ce40418b08ac9
Apurva107 <apurvapatil71@gmail.com>
2025-12-01 18:27:29 +0100

    add

---
+++ print().py
@@ -0,0 +1,7 @@
+# PRINT()
+#It is a built-in python function that displays message on the output screen to communicate with users.
+#Use cases: Communicate, Show results, Debugg, Test.
+
+print("Hi World") # Double quotes
+print('Hello World') # Single quotes
+# print("Hi') this gives error
