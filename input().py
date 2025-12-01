c189c25b56212706021f54a4014e3c4b75a04aeb
Apurva107 <apurvapatil71@gmail.com>
2025-12-01 19:03:21 +0100

    add

---
+++ input().py
@@ -0,0 +1,11 @@
+#INPUT()
+#We use print() function to display something to users, Likewise we use input() function to get something from user.
+#It is the built-in function that stops your programm to get user input.
+
+# input("Enter your Name:") example 
+
+#Variables & INPUT()
+ 
+name = input("Enter your Name:") # here "name" is Dynamic value which means data entered by the user that can vary each time the program runs.
+Age = 27 # here "age" is static value or hard coded value - fixed peice of data wrritten directly into your code that never changes at runtime.
+print(name, "my age is", Age)
