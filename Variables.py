4a821d8ef2484e038e83b89cc17c8f946d968f1f
Apurva107 <apurvapatil71@gmail.com>
2025-12-01 18:47:41 +0100

    add

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#Variables in Python
+#Its the container for storing the data value, place holder in memory.
+#Give a variable a value using the assignment operator `=`.
+#Variables name are case sensitive.
+
+x = 5 # x - variable name , 5 - value
+y = "Hello" # y - variable name , Hello - value
+print(x)
+print(y)
+
+#EXAMPLE 2
+# Store your name as variable and reuse it in print().
+name = "Python"
+print("My name is", name)
+print(name, "is simple and understandable")
