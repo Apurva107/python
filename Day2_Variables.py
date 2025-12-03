884eac161883ac2fbf842a79287f731551c4bdac
Apurva107 <apurvapatil71@gmail.com>
2025-12-03 13:39:40 +0100

    add

---
+++ Day2_Variables.py
@@ -0,0 +1,110 @@
+#Exercises: Level 1
+
+# Declare a first name variable and assign a value to it
+#Declare a last name variable and assign a value to it
+#Declare a full name variable and assign a value to it
+# Declare a country variable and assign a value to it
+# Declare a city variable and assign a value to it
+# Declare an age variable and assign a value to it
+# Declare a year variable and assign a value to it
+#Declare a variable is_married and assign a value to it
+#Declare a variable is_true and assign a value to it
+#Declare a variable is_light_on and assign a value to it
+#Declare multiple variable on one line
+
+first_name = "Apurva"
+last_name = "p"
+full_name = "apurva p"
+country = "india"
+city = "bangalore"
+age = 25
+year = 2025
+is_married = True
+is_true = "Y"
+is_light_on = "No"
+
+#Declaring multiple variable on one line
+first_name,last_name,full_name,country,city,age,year,is_married = 'Apurva','p','apurva p','india','bangalore',25,2025,True
+print("first name:",first_name)
+print("last name:",last_name)
+print("full name:",full_name)
+print("Country:",country)
+print("City:",city)
+print("Age:",age)
+print("Year:",year)
+print("Married:",is_married)
+
+#LEVEL 2
+#Check the data type of all your variables using type() built-in function
+#Using the len() built-in function, find the length of your first name
+#Compare the length of your first name and your last name
+#Declare 5 as num_one and 4 as num_two
+#Add num_one and num_two and assign the value to a variable total
+#Subtract num_two from num_one and assign the value to a variable diff
+#Multiply num_two and num_one and assign the value to a variable product
+#Divide num_one by num_two and assign the value to a variable division
+#Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+#Calculate num_one to the power of num_two and assign the value to a variable exp
+#Find floor division of num_one by num_two and assign the value to a variable floor_division
+#The radius of a circle is 30 meters.
+#Calculate the area of a circle and assign the value to a variable name of area_of_circle
+#Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+#Take radius as user input and calculate the area.
+#Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable 
+
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(is_married))
+
+print(len(first_name))
+
+len_first = len(first_name)
+len_last = len(last_name)
+print("the length of first name:",len_first)
+print("the length of last name:",len_last)
+
+num_one = 5
+num_two = 4
+
+variable_total = num_one + num_two
+print("Variable total :",variable_total)
+
+variable_diff = num_two - num_one
+print("Variable diff :",variable_diff)
+
+variable_product = num_two*num_one
+print("Variable product :",variable_product)
+
+variable_division = num_one/num_two
+print("Variable division :",variable_division)
+
+variable_remainder = num_two % num_one
+print("Variable remainder :",variable_remainder)
+
+variable_exp = num_one**num_two
+print("Variable exp :",variable_exp)
+
+floor_division = num_one//num_two
+print("Floor Division :",floor_division)
+
+r = 30
+area_of_circle = 3.14*(r)**2
+print(area_of_circle)
+
+circum_of_circle = 2*3.14*r
+print(circum_of_circle)
+
+
+radius = float(input("enter the radius of the circle:"))
+area_of_circle = 3.14*radius*radius
+print("area of circle:",area_of_circle)
+
+first_name = input("enter first name:")
+last_name = input("enter last name:")
+country = input("enter country name:")
+age = input("enter the age:")
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Country:",country)
+print("Age:",age)
